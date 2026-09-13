9eaaac1077c5bcc70466122ee98029791448390e
SHAILEY <shailendrameghwal07@gmail.com>
2026-09-13 22:39:31 +0530

    Move PORT declaration to the end of the file

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-const PORT = process.env.PORT || 5000;
+
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
@@ -36,6 +36,8 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api', apiRoutes);
+
+const PORT = process.env.PORT || 5000;
 
 const startServer = async () => {
   await connectDB();
